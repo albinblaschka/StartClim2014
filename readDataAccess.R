c23bdd07e2518aa2b631060eaf3a2121636e5b63
Albin Blaschka <albin.blaschka@raumberg-gumpenstein.at>
2015-07-30 15:39:42 +0200

    Datenmanagement: Testcode für Lesen aus .accdb - Datei (Access)

---
+++ readDataAccess.R
@@ -0,0 +1,12 @@
+
+
+setwd("Y:/Institut3/AGRAM/StartClim2014/Albin/analyse")
+
+library(RODBC) 
+
+
+mdbConnect <- odbcConnectAccess2007('Vegetation_StartClim2014.accdb') 
+odbcGetInfo(mdbConnect)
+
+sqlTables(mdbConnect)
+dta <- sqlFetch(mdbConnect, 'tbl_sobotik') 
