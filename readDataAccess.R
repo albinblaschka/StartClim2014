269bee24ab34d65865a3b54f722db8c61221599a
Albin Blaschka <albin.blaschka@raumberg-gumpenstein.at>
2015-08-11 15:28:39 +0200

    Tests zum taxize - Paket

--- readDataAccess.R
+++ readDataAccess.R
@@ -3,12 +3,18 @@
 setwd("Y:/Institut3/AGRAM/StartClim2014/Albin/analyse")
 
 library(RODBC) 
-
+library(taxize)
 
 mdbConnect <- odbcConnectAccess2007('Vegetation_StartClim2014.accdb') 
 odbcGetInfo(mdbConnect)
+# sqlTables(mdbConnect)
 
-sqlTables(mdbConnect)
-dta <- sqlFetch(mdbConnect, 'tbl_sobotik') 
-
+qry <- sqlQuery(mdbConnect, "SELECT DISTINCT species FROM tbl_sobotik", errors = TRUE)
+head(qry)
 odbcClose(mdbConnect)
+
+res <- gnr_resolve(names = qry$species, data_source_ids = '158')
+res
+
+correct <- tnrs(query = qry$species, source = "iPlant_TNRS")[ , -c(5:7)]
+correct
